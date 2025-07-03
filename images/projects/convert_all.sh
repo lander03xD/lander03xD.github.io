25a2567ffa55fc1149b40b9a7ee0bce2d41c5eb5
Lander03xD <22810909+lander03xD@users.noreply.github.com>
2025-07-03 09:04:12 +0200

    autorotate for image convert

--- images/projects/convert_all.sh
+++ images/projects/convert_all.sh
@@ -1,5 +1,5 @@
 for file in *; 
 do 
 echo "Processing $file"; 
-convert "$file" -resize x1000 -strip -quality 85 "./web/$(basename "${file%.*}").webp"; 
+convert "$file" -resize x1000 -strip -auto-orient -quality 85 "./web/$(basename "${file%.*}").webp"; 
 done
